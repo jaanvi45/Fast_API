04730484e7cd0943c6ab00ff4d591bf6470c5944
jaanvi45 <janvi.vikartrtechnologies@gmail.com>
2025-07-14 18:02:17 +0530

    Created API !!

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {'data':{'name': 'Janvi'}}
+@app.get("/blog")
+def index(limit=10,published: bool=True,sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs form the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
 
 
 @app.get("/about")
 def about():
     return{'data':'about page'}
 
-@app.get("/greet/{name}")
-def greet_user(name: str):
+@app.get("/greeting/{name}")
+def greeting(name: str):
     return {"greeting": f"Hello, {name}!"}
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": " All Unpublished blogs"}
+
+@app.get("/blog/{id}")
+def blog(id: int):
+    return {"data": id}
+
+@app.get("/blog/{id}/comments")
+def comments(id: int):
+    return {"data": f"Comments for blog {id}"}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool] = None
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
+@app.post("/blog")
+def create_blog(request: Blog):
+    return {"data": f"Blog is created with title as {request.title}"}
